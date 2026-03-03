d8930f9a0002873828fd3608b17cd78725c22328
clementine-mailsoar <clementine@mailsoar.com>
2026-03-03 18:46:32 +0100

    Correction canonical pour SEO

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Inbox by MailSoar')</title>
-    <link rel="canonical" href="{{ url()->current() }}" />
+    <link rel="canonical" href="{{ url()->full() }}" />
 
     <!-- Meta tags for SEO -->
     @yield('meta')
