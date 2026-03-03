108b9f5710a8f9c042cb07d365775cf2537cb05c
clementine-mailsoar <clementine@mailsoar.com>
2026-03-03 18:39:06 +0100

    Ajout canonical URL - SEO

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Inbox by MailSoar')</title>
-    
+    <link rel="canonical" href="{{ url()->current() }}" />
+
     <!-- Meta tags for SEO -->
     @yield('meta')
     
